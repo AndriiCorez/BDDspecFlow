6d50243ce3120a292b203fe89646750a6292aa2a
Andrii Vasyliev <andrii.vasyliev@gft.com>
2016-02-26 14:53:42 +0100

    Init commit

---
+++ BDDexample/Features/GoogleSearch.feature
@@ -0,0 +1,15 @@
+﻿Feature: GoogleSearch
+	Feature to test Google page search functionality
+	Auto-search and navigation to search result
+
+@SmokeTest
+@Browser:Chrome
+Scenario: Google Saerch for Wikipedia
+	Given I have navigated to Google page
+	And I see the Google page fully loaded
+	When I type search keyword as
+	|Keyword	|
+	| wikipedia |
+	Then is hould see the result for keyword
+	|Keyword	|
+	| wikipedia |
---
+++ BDDexample/Steps/GoogleSearchSteps.cs
@@ -0,0 +1,53 @@
+﻿using Baseclass.Contrib.SpecFlow.Selenium.NUnit.Bindings;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Configuration;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace BDDexample.Steps
+{
+    [Binding]
+    public class GoogleSearchSteps
+    {
+        IWebDriver _driver;
+
+        public GoogleSearchSteps()
+        {
+            _driver = Browser.Current;
+            _driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+        }
+
+        [Given(@"I have navigated to Google page")]
+        public void GivenIHaveNavigatedToGooglePage()
+        {
+            Browser.Current.Navigate().GoToUrl(ConfigurationManager.AppSettings["seleniumBaseUrl"]);
+        }
+        
+        [Given(@"I see the Google page fully loaded")]
+        public void GivenISeeTheGooglePageFullyLoaded()
+        {
+            if (_driver.FindElement(By.Name("q")).Displayed == true)
+                Console.WriteLine("Loaded");
+            else
+                Console.WriteLine("Failed to load");
+        }
+        
+        [When(@"I type search keyword as")]
+        public void WhenITypeSearchKeywordAs(Table table)
+        {
+            dynamic tableDetails = table.CreateDynamicInstance();
+            _driver.FindElement(By.Name("q")).SendKeys(tableDetails.Keyword);
+        }
+        
+        [Then(@"is hould see the result for keyword")]
+        public void ThenIsHouldSeeTheResultForKeyword(Table table)
+        {
+            dynamic tableDetails = table.CreateDynamicInstance();
+            string value = tableDetails.Keyword;
+            if (_driver.FindElement(By.PartialLinkText(value)).Displayed == false)
+                Assert.Fail(value + " control not found");
+        }
+    }
+}
